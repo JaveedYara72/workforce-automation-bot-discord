8fa42ad79be1e0305a7f213c3381fb08315e056a
KanayBhandari <53465075+KanayBhandari@users.noreply.github.com>
2020-09-07 21:21:26 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -7,12 +7,34 @@ CAREER_ID = 743395784460075071
 PARTNER_ID = 743395673545769021
 COMMUNITY_ID = 743395897173344336
 
-# FOR DATABASE CONNECTION
-HOST = 'localhost'
-USER = 'root'
-PASSWORD = ''
-DATABASE = 'ticket'
+BIRTHDAY_ID = 751064249325060107
+TASK_UPDATES_CHANNEL_ID = 749977444676665354
+ACCEPTED_TASK_CHANNEL_ID = 750230418326028299
+TASK_EDIT_CHANNEL_ID = 752495183697936395
 
+################################################################################################################
+# FOR DATABASE CONNECTION
+################################################################################################################
+
+HOST = "localhost"
+DATABASE = "ticket"
+USER = "root"
+PASSWORD = ""
+PORT = 3306
+
+
+################################################################################################################
 # FOR SENDING MESSAGE LOGS AND EXCEPTION LOGS
+################################################################################################################
 MESSAGE_WEBHOOK = "https://discordapp.com/api/webhooks/745592902322749500/_o5cCah0cxsh53lPTIXJfrujo5w4kTGmy41WdeEr43wz0nUFPkMxLNUrjDE-wM_4pgpC"
 EXCEPTION_WEBHOOK = "https://discordapp.com/api/webhooks/745646282504405142/xd6Xcbc16I5is6TKc6XSFvAjt9pkEK2HboOogKFWq1w3WbpTpFISdvryfKtYHkrdQZcs"
+
+
+################################################################################################################
+# MESSAGE ID 
+################################################################################################################
+PARTNER_EMBED_ID = 743395678948032532
+CAREER_AT_KODERS_EMBED_ID = 743395789061226516
+COMMUNITY_EMBED_ID = 743395901657317416
+PROJECT_REGISTRATION_EMBED_ID = 743396000932036650
+CLIENT_REGISTRATION_EMBED_ID = 747779843608805387
