e39716db94a18a1ded4316b1fd48d65296f98d5a
KanayBhandari <53465075+KanayBhandari@users.noreply.github.com>
2020-09-10 19:08:46 +0530

    switch to sqlite database

--- leveling_system.py
+++ leveling_system.py
@@ -1,38 +1,56 @@
 import discord
-import mysql.connector
+import sqlite3
+
+###############################################################################################################
+# MANUAL IMPORT
+###############################################################################################################
 import settings as setting
 
 
+###############################################################################################################
+# DATABASE CONNECTION
+###############################################################################################################
+db_file = "demo.db"
+try:
+	mydb = sqlite3.connect(db_file)
+	mycur = mydb.cursor()
+except Exception as e:
+	print(e)
+
+
+###############################################################################################################
+# DATABASE QUERIES
+###############################################################################################################
+def update(update_query, value):
+	mycur.execute(update_query, value)
+	mydb.commit()
+
+###############################################################################################################
+# LEVEL SYSTEM
+###############################################################################################################
 async def add(ctx, assigned_to):
-
-	mydb = mysql.connector.connect(host=setting.HOST, port=setting.PORT, database=setting.DATABASE, user=setting.USER, password=setting.PASSWORD)
-	mycur = mydb.cursor(buffered=True)
-
-	def update(update_query, value):
-		mycur.execute(update_query, value)
-		mydb.commit()
 
 	try:
 		total_xp = 0
-		mycur.execute("select Given_XP from task where Assigned_To = %s", (assigned_to, ))
+		mycur.execute("select Given_XP from task where Assigned_To = ?", (assigned_to, ))
 		for row in mycur:
 			total_xp = row[0] + total_xp
 
 
-		mycur.execute("select Level from internal where Discord_Username = %s", (assigned_to, ))
+		mycur.execute("select Level from internal where Discord_Username = ?", (assigned_to, ))
 		row = mycur.fetchone()
 		start_level = row[0]
 		end_level = int((total_xp)**(1/4))
 
 
 
-		update_query = "update internal set Total_XP = %s where Discord_Username = %s"
+		update_query = "update internal set Total_XP = ? where Discord_Username = ?"
 		value = (total_xp, assigned_to)
 		update(update_query, value)
 
 		if start_level < end_level:
 			level = end_level
-			update_query = "update internal set Level = %s where Discord_Username = %s"
+			update_query = "update internal set Level = ? where Discord_Username = ?"
 			value = (level, assigned_to)
 			update(update_query, value)
 		await ctx.send("Task completed.")
