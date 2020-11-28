5a4535ff2ac7f299dc4f5c3c682af45ffb0ad589
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-11-28 20:45:20 +0530

    ➕ Added vision command

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 
 # Manual file imports
 import config as CONFIG # Capitals for global
+import embeds as EMBEDS 
 
 bot = commands.Bot(command_prefix="~")
 
@@ -15,6 +16,7 @@ bot = commands.Bot(command_prefix="~")
 async def on_ready(): # Triggers when bot is ready
     logging.info("Kourage is running at version {0}".format(CONFIG.VERSION))
 
+# Ping command
 @bot.command()
 @commands.has_any_role("@everyone")
 async def ping(msg):
@@ -25,10 +27,16 @@ async def ping(msg):
               'Username: {0} \n '.format(msg.author.name) +
               'Latency: {0} sec '.format(round(bot.latency, 1)))
 
+@bot.command()
+@commands.has_any_role("Kore")
+async def vision(msg):
+    embed = EMBEDS.vision()
+    await msg.send(embed=embed)
+
 if __name__ == "__main__":
     try:
         bot.run(CONFIG.TOKEN)
     except Exception as e:
-        logging.error("Exception found at main worker", exc_info=True)
+        logging.warning("Exception found at main worker", exc_info=True)
 
     
