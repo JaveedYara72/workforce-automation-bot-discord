9bdeced45b924ab478886b7d3e617700acb52f7d
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-11-28 18:49:34 +0530

    ➕ Added ping command

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ import config as CONFIG # Capitals for global
 bot = commands.Bot(command_prefix="~")
 
 @bot.event
-async def on_ready():
+async def on_ready(): # Triggers when bot is ready
     logging.info("Kourage is running at version {0}".format(CONFIG.VERSION))
+
+@bot.command()
+async def ping(msg):
+    await msg.send('Pong! 🏓\n ' + 
+              'Name: Kourage \n ' +
+              'Description: AIO bot of Koders \n ' +
+              'Version: {0} \n '.format(CONFIG.VERSION) +
+              'Username: {0} \n '.format(msg.author.name) +
+              'Latency: {0} sec '.format(round(bot.latency, 1)))
 
 if __name__ == "__main__":
     try:
         bot.run(CONFIG.TOKEN)
     except Exception as e:
-        logging.error("MAIN RUN EXCEPTION", exc_info=True)
+        logging.error("Exception found at main worker", exc_info=True)
 
     
