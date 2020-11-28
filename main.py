57b08d1d412630ae015a078e7395dd89e9ab2b98
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-11-28 09:27:50 +0530

    📋 Logger Added

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import discord
 from discord.ext import commands, tasks
 import asyncio
+
+# LOGGING WITH FORMAT
 import logging
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 
 # Manual file imports
 import config as CONFIG # Capitals for global
 
-bot = commands.Bot(command_prefix="&")
+bot = commands.Bot(command_prefix="~")
 
 @bot.event
 async def on_ready():
     logging.info("Kourage is running at version {0}".format(CONFIG.VERSION))
-    print("Kourage is running at version {0}".format(CONFIG.VERSION))
 
-bot.run(CONFIG.TOKEN)
+if __name__ == "__main__":
+    try:
+        bot.run(CONFIG.TOKEN)
+    except Exception as e:
+        logging.error("MAIN RUN EXCEPTION", exc_info=True)
+
     
