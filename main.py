e8ab58b6a7386243b019e586f33dfc4117cc298f
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-11-30 10:32:27 +0530

    ➕Added requirements.txt

--- main.py
+++ main.py
@@ -34,6 +34,21 @@ async def vision(msg):
     embed = EMBEDS.vision()
     await msg.send(embed=embed)
 
+# Remind command
+@bot.command()
+@commands.has_any_role("Koders")
+async def remind(ctx, *args):
+    await ctx.message.delete()
+    await asyncio.sleep(float(args[0]) * 60 * 60)
+    author = ctx.author.name
+    embed = discord.Embed(title="{0}".format(args[1]), color=0x2459bc)
+    embed.set_author(name="Auto Reminder!")
+    embed.set_thumbnail(url="https://www.flaticon.com/svg/static/icons/svg/2919/2919780.svg")
+    embed.add_field(name="Invoked by ", value="{0}".format(author), inline=True)
+    embed.add_field(name="Invoked for ", value="{0}".format(args[2]), inline=True)
+    embed.set_footer(text="Made with ❤️️  by Koders")
+    await ctx.send(embed=embed)
+
 if __name__ == "__main__":
     try:
         bot.run(CONFIG.TOKEN)
