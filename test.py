b2ab69dd15b0f84d81f054e2b0f708552428e37f
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-12-07 01:15:08 +0530

    🛈 Handled Exception at JSON parse (define)

--- test.py
+++ test.py
@@ -7,12 +7,14 @@ headers = { "Authorization": "Token c5a6932805bff0c739cd87d45dd8530d88c064bd" }
 try:
     response = requests.get(url, headers=headers)
 except Exception as e:
-    print("Something went wrong: " + e)
+    print("Something went wrong during requesting API. Reason: " + e)
 
 # PARSE JSON
 data = json.loads(response.text)
-print(data['definitions'])
-print(data['definitions'][0]['type'])
-print(data['definitions'][0]['definition'])
-print(data['definitions'][0]['example'])
-
+try:
+    print(data['definitions'])
+    print(data['definitions'][0]['type'])
+    print(data['definitions'][0]['definition'])
+    print(data['definitions'][0]['example'])
+except:
+    print("Something went wrong during parsing JSON. Reason: " + e)
