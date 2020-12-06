dcdc40f8abcf926c34e7b53371046ed4a678fd12
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-12-07 01:07:24 +0530

    ❕ Handled excepttions

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests
-import JSON
 
 url = 'https://owlbot.info/api/v4/dictionary/fruit salad' 
 headers = { "Authorization": "Token c5a6932805bff0c739cd87d45dd8530d88c064bd" }
-response = requests.get(url, headers=headers)
+try:
+    response = requests.get(url, headers=headers)
+except Exception as e:
+    print("Something went wrong: " + e)
 print(response.text)
