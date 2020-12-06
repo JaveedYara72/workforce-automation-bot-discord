5bb514143030cb2ef6d90f6ec6f1a7224bf6f675
Xhunter <kushwah.kartikey14@stu.upes.ac.in>
2020-12-07 01:12:45 +0530

    🔍 Filtered JSON

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 import requests
+import json
 
+# API REQUEST
 url = 'https://owlbot.info/api/v4/dictionary/fruit salad' 
 headers = { "Authorization": "Token c5a6932805bff0c739cd87d45dd8530d88c064bd" }
 try:
     response = requests.get(url, headers=headers)
 except Exception as e:
     print("Something went wrong: " + e)
-print(response.text)
+
+# PARSE JSON
+data = json.loads(response.text)
+print(data['definitions'])
+print(data['definitions'][0]['type'])
+print(data['definitions'][0]['definition'])
+print(data['definitions'][0]['example'])
+
